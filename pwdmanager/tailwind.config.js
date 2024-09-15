9794b5dfb510fcfb3596da07f34d52829901b2ac
2055619 <2055619@claurendeau.qc.ca>
2024-09-15 17:52:36 -0400

    Web part

---
+++ pwdmanager/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'pwdm-one': 'var(--pwdm-one-color)',
+        'pwdm-two': 'var(--pwdm-two-color)',
+        'pwdm-three': 'var(--pwdm-three-color)',
+        'pwdm-four': 'var(--pwdm-four-color)',
+        'pwdm-five': 'var(--pwdm-five-color)',
+      },
+    },
+  },
+  plugins: [],
+}
