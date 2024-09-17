dc78b75e03080efbaaa49ec733590dc7930a6cfa
2055619 <2055619@claurendeau.qc.ca>
2024-09-16 22:58:04 -0400

    Read me

--- pwdmanager/src/components/pages/Main.tsx
+++ pwdmanager/src/components/pages/Main.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {Route, Routes, useNavigate} from "react-router-dom";
 import PageNotFound from "../utils/PageNotFound";
 import SignIn from "../auth/SignIn";
@@ -6,13 +6,33 @@ import SignUp from "../auth/SignUp";
 import {IUser} from "../../assets/models/Authentication";
 import Home from "./Home";
 import Header from "../utils/Header";
+import {PwdmanagerServerInstance} from "../../App";
+import {UserService} from "../../services/UserService";
 
 function Main() {
+    const navigate = useNavigate();
+    const userService = new UserService();
     const [user, setUser] = useState<IUser | null>(null);
 
     function handleSignOut() {
         setUser(null);
     }
+
+    useEffect(() => {
+        const token = sessionStorage.getItem('token');
+        if (token && user === null) {
+            userService.getMe().then(response => {
+                setUser(response);
+                PwdmanagerServerInstance.defaults.headers.common['Authorization'] = "Bearer " + token;
+            })
+        }
+        // if (token === undefined || token === null) {
+        //     setUser(null);
+        //     localStorage.removeItem('token');
+        //     PwdmanagerServerInstance.defaults.headers.common['Authorization'] = '';
+        //     navigate("/");
+        // }
+    }, [user]);
 
 
     return (
