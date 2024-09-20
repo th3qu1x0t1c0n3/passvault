86713f104a3d211bb34da89ff8cd02c742466532
2055619 <2055619@claurendeau.qc.ca>
2024-09-19 20:43:55 -0400

    Touch up

--- pwdmanager/src/App.tsx
+++ pwdmanager/src/App.tsx
@@ -33,6 +33,7 @@ function App() {
 export default App;
 
 export const PwdmanagerServerInstance = axios.create({
+    // baseURL: 'https://passvault.quixotic.date:8087/api/v1',
     baseURL: 'http://localhost:8081/api/v1',
     headers: {
         'Content-Type': 'application/json',
