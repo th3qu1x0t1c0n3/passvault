aa72138727b00cb2a7706ec146014dd7f98bd993
2055619 <2055619@claurendeau.qc.ca>
2024-10-01 21:34:28 -0400

    Start extension

--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React a
+          Learn React weef
         </a>
       </header>
     </div>
