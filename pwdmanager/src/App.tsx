e53c222d0e149b6fc3ccc57a4df76882ae3b0b4b
2055619 <2055619@claurendeau.qc.ca>
2024-12-11 16:26:40 -0500

    Docker

--- pwdmanager/src/App.tsx
+++ pwdmanager/src/App.tsx
@@ -34,8 +34,6 @@ export default App;
 
 export const PwdmanagerServerInstance = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
-    // baseURL: 'https://passvault.quixotic.date/api/v1',
-    // baseURL: 'http://localhost:8087/api/v1',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
